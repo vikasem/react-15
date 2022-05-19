103f993b48ff44a19ccba680e804e0876f8ee6e6
Semenova Vika <vika_semenova_11@list.ru>
2022-05-19 21:31:05 +0300

    avatar

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,8 +3,8 @@ import MyPosts from './MyPosts/MyPosts.jsx';
 import classes from './Profile.module.css';
 let Profile = () => {
     return (
-        <div>
-            <img className={classes.panorama} src='https://c.wallhere.com/photos/84/7f/1600x900_px_landscape-1057343.jpg!d' />
+        <div className = {classes.content}>
+            <img src='https://c.wallhere.com/photos/84/7f/1600x900_px_landscape-1057343.jpg!d' />
             <div>
                 ava+content
             </div>
