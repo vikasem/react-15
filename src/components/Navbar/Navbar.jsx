abe7418db9dff32185e7a62658a01f12eab40bd8
Semenova Vika <vika_semenova_11@list.ru>
2022-05-24 19:11:13 +0300

    страница диалогов

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './Navbar.module.css';
 let Navbar = () => {
     return (
         <nav className={classes.nav}>
             <div className={classes.item}>
-                <a href='/profile'>Profile</a>
+                <NavLink to='/profile'>Profile</NavLink>
             </div>
             <div className={classes.item}>
-                <a  href='/dialogs'>Message</a>
+                <NavLink  to='/dialogs'>Message</NavLink>
             </div>
             <div className={classes.item}>
                 <a>News</a>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import MyPosts from './MyPosts/MyPosts.jsx';
 import classes from './Profile.module.css';
+import ProfileInfo from './ProfileInfo/ProfileInfo.jsx';
 let Profile = () => {
     return (
         <div>
-            <img className={classes.panorama} src='https://c.wallhere.com/photos/84/7f/1600x900_px_landscape-1057343.jpg!d' />
-            <div>
-                ava+content
-            </div>
+    
+            <ProfileInfo />
             <MyPosts />
 
         </div>
